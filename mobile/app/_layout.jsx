28a329c3e22656304b0599c899ae1f2a7163a34a
Krushna <fullstack.kanha@gmail.com>
2025-05-15 16:18:48 +0530

    Add layout components, styles, and color constants for the mobile app

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Image } from "expo-image";
 
 const RootLayout = () => {
   return (
     <View>
       <Text>RootLayout</Text>
+      <Text>Hello World</Text>
     </View>
   );
 };
